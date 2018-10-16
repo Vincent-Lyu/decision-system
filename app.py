fd3625bbff66b32a00016d553228741bd39a3592
Vincent-Lyu <wenxiang.lyu@gmail.com>
2018-10-16 20:59:49 +0800

    科研统计

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 app = Flask(__name__)
 
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+keyanchengguo_df = pd.DataFrame.from_csv('{}/data/教师科研成果.csv'.format(CURR_DIR))
+keyanchengguo_df = keyanchengguo_df.fillna('')
+
 class WordCloudD3DTO:
     text = ""
     size = 0
@@ -52,20 +56,52 @@ def getWordCloudData():
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', curr_active='学院概况')
 
 @app.route('/教师绩效', methods=['GET'])
 def jiaoshijixiao():
     wordCloudData = getWordCloudData()
-    return render_template('index2.html', wordCloudData=wordCloudData)
+    return render_template('404.html', wordCloudData=wordCloudData, curr_active='教师绩效')
 
 @app.route('/教师科研统计', methods=['GET'])
 def jiaoshikeyantongji():
-    return render_template('404.html')
+    df = keyanchengguo_df
+
+    headers = list(df.columns)
+    rows = []
+    for index in df.index:
+        row = [index]
+        for col in df.columns:
+            row.append(df.loc[index][col])
+        rows.append(row)
+
+    res = []
+    total = 0
+    for col in df.columns:
+        series = [i for i in df[col] if isinstance(i, float)]
+        res.append((col, sum(series)))
+        total += sum(series)
+
+    sorted_res = sorted(res, key=lambda t: t[1], reverse=True)
+    
+    scores = []
+    for index in df.index:
+        score = [df.loc[index][col] for col in df.columns if isinstance(df.loc[index][col], float)]
+        scores.append((index, sum(score)))
+
+    sorted_scores = sorted(scores, key=lambda t: t[1], reverse=True)
+
+    return render_template('keyantongji.html', total=total,
+                            rank1_item=sorted_res[0][0], rank1_score=sorted_res[0][1],
+                            rank2_item=sorted_res[1][0], rank2_score=sorted_res[1][1],
+                            rank3_item=sorted_res[2][0], rank3_score=sorted_res[2][1],
+                            headers=headers, rows=rows, 
+                            items=[i for i, s in sorted_res], sum=[s for i, s in sorted_res],
+                            names=[i for i, s in sorted_scores], scores=[s for i, s in sorted_scores], curr_active='教师科研统计')
 
 @app.route('/教师画像', methods=['GET'])
 def jiaoshihuaxiang():
-    return render_template('404.html')
+    return render_template('404.html', curr_active='教师画像')
 
 
 if __name__ == '__main__':
